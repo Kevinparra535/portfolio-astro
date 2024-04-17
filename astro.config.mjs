2a5c316f35c2686ae4226142bcfe57d9589e9456
Kevin Parra <kevinparra535@gmail.com>
2024-04-17 16:32:42 -0500

    [TESTING] TRANSLATIONS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,29 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
+import SpanishLanguage from "./src/config/locales/spanish.json";
+import EnglishLanguage from "./src/config/locales/english.json";
+
+const resources = {
+  en: {
+    translation: EnglishLanguage,
+  },
+  es: {
+    translation: SpanishLanguage,
+  },
+};
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    integrations: true,
+  },
+  integrations: [
+    astroI18next({
+      resources,
+      baseLanguage: "en",
+      i18next: {
+        debug: true, // convenient during development to check for missing keys
+        supportedLngs: ["en", "fr"],
+      },
+    }),
+  ],
+});
