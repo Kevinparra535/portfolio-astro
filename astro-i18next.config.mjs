1ac249328ef1e21d7b2e610d2b600b2f5e85c6eb
Kevin Parra <kevinparra535@gmail.com>
2024-04-18 16:41:08 -0500

    [ADD] NEW SCREENS AND ROUTES

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,5 +1,14 @@
-/** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
-  defaultLocale: "en",
+  defaultLocale: "es",
   locales: ["en", "es"],
+  routes: {
+    es: {
+      about: "quiensoy",
+      works: "trabajos",
+    },
+    en: {
+      about: "about",
+      works: "works",
+    },
+  },
 };
