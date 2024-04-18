b5d525c70a2c0983e16f931daf2311d8a194a37e
Kevin Parra <kevinparra535@gmail.com>
2024-04-18 16:03:38 -0500

    [ADD] NEW HERO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next()],
+  integrations: [astroI18next(), icon()]
 });
